c9753196e9aba19192b75622eb668c12e7f3b0ed
Anthony Lorendeaux <anthony.lorendeaux@free.fr>
2021-02-06 09:52:18 +0100

    :tada: Initial commit
    Add basic strapi architecture and collections.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c3ed8c4f26d0e9f67264f6cda642a0c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1fc83462-15dc-4af3-9f9a-00c1cd25e73b'
+};
