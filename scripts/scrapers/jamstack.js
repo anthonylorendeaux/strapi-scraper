40fd13b2521ed08807cb047d34fdb5208cc8c104
Anthony Lorendeaux <anthony.lorendeaux@free.fr>
2021-02-06 14:50:40 +0100

    :construction: Update the jamstack.js
    Update jamstack.js file and fix a mistake in utils.js file.

--- scripts/scrapers/jamstack.js
+++ scripts/scrapers/jamstack.js
@@ -1,15 +1,40 @@
+'use strict'
+
+const chalk = require('chalk');
+const puppeteer = require('puppeteer');
+const {
+  getReport,
+  getDate,
+  getAllSG,
+  scraperCanRun
+} = require('./utils/utils.js')
+
+let report = {}
+let errors = []
+let newSG = 0
+
+const scrape = async () => {
+  console.log("Scrape function");
+}
+
 const main = async () => {
-    // Fetch the correct scraper thanks to the slug
-    const slug = "jamstack-org"
-    const scraper = await strapi.query('scraper').findOne({
-      slug: slug
-    });
-    
-    console.log(scraper);
-  
-    // If the scraper doesn't exists, is disabled or doesn't have a frequency then we do nothing
-    if (scraper == null || !scraper.enabled || !scraper.frequency)
-      return
-  }
-  
-  exports.main = main;
+  // Fetch the correct scraper thanks to the slug
+  const slug = "jamstack-org"
+  const scraper = await strapi.query('scraper').findOne({
+    slug: slug
+  });
+
+  // If the scraper doesn't exists, is disabled or doesn't have a frequency then we do nothing
+  // if (scraper == null || !scraper.enabled || !scraper.frequency){
+  //   console.log(`${chalk.red("Exit")}: (Your scraper may does not exist, is not activated or does not have a frequency field filled in)`);
+  //   return
+  // }
+
+  // const canRun = await scraperCanRun(scraper);
+  // if (canRun && scraper.enabled){
+    const allSG = await getAllSG(scraper)
+    await scrape(allSG, scraper)
+  // }
+}
+
+exports.main = main;
--- scripts/scrapers/utils/utils.js
+++ scripts/scrapers/utils/utils.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const parser = require('cron-parser');
+const chalk = require('chalk');
 
 const scraperCanRun = async (scraper) => {
   const frequency = parser.parseExpression(scraper.frequency);
